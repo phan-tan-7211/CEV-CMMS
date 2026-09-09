b6309b46f266f3fe705191f50ede9ef36d55e0a5
phan-tan-7211 <116583632+phan-tan-7211@users.noreply.github.com>
2026-09-09 07:46:33 +0700

    feat(mobile): export preventive maintenance feature API

---
+++ apps/mobile/src/features/preventive-maintenance/index.ts
@@ -0,0 +1,7 @@
+export {
+  generatePmWorkOrder,
+  listMeters,
+  listPmSchedules,
+  recordMeterReading,
+} from './api/preventiveMaintenanceService'
+export type { MeterItem, PmDueItem } from './api/preventiveMaintenanceService'
