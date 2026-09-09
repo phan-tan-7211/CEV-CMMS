b15330d6d6f9e6c430eeb6deb5720f9332d37491
phan-tan-7211 <116583632+phan-tan-7211@users.noreply.github.com>
2026-09-09 18:18:24 +0700

    feat(inventory): export inventory feature

---
+++ apps/mobile/src/features/inventory/index.ts
@@ -0,0 +1,9 @@
+export {
+  loadInventorySnapshot,
+  moveInventory,
+  type InventoryPart,
+  type InventorySnapshot,
+  type PartStockRow,
+  type StockLocation,
+  type StockTransaction,
+} from './api/inventoryService'
